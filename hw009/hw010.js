f1391832accbb31ca3a98dd4c83448cd86fcdd6d
a-harbuz <145448459+a-harbuz@users.noreply.github.com>
2024-03-20 20:32:35 +0100

    Add files via upload

---
+++ hw009/hw010.js
@@ -0,0 +1,36 @@
+//В рамках БД "190923_MUSIC" напишите след/запросы:
+
+//Вывести заголовки статей без тега test
+
+db.articles.find (
+    {tags:{$ne:'test'}},
+    {title:1, _id:0}
+)
+
+//Вывести статьи у которых есть теги spring и joy (оба тега)
+
+db.articles.find (
+    {tags:{$in:['spring', 'joy']}},
+    {_id:0}
+)
+
+//Удалить у всех статей теги test1 и test2
+
+db.articles.updateMany (
+    {},
+    {
+        $pullAll:{
+                tags:['test1','test2']
+              }
+    }
+)
+
+
+//Добавить к статьям с тегом spring тег awesome и поле is_checked со значением true
+
+db.articles.updateMany (
+    {tags:'spring'},
+    {$push:{tags:'awesome'},
+     $set:{is_checked: true}
+    }
+)
